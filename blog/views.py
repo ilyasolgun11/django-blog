93759abc4c1be47bcf7f21fd53586eee5eaba09c
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-04-22 18:47:17 +0100

    Edit base.html link

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
+    queryset = Post.objects.all()
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 9
