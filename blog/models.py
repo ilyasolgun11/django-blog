ebaabc052a5dee1e0b706e2d8c428c98a039ebd1
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-04-22 13:47:57 +0100

    Add generic post view

--- blog/models.py
+++ blog/models.py
@@ -15,8 +15,10 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
+
     def __str__(self):
         return f"Title {self.title} | Written by {self.author}"
 
