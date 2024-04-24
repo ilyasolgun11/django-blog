8a4c8493e3a865f8210b8248fdd82411e8fc8034
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-04-24 16:49:18 +0100

    Add authentication

--- about/views.py
+++ about/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import About
 
 # Create your views here.
@@ -10,9 +10,7 @@ def about_me(request):
     about = About.objects.all().order_by('-updated_on').first()
 
     return render(
-        request, 
+        request,
         "about/about.html",
         {"about": about}
     )
-
-
